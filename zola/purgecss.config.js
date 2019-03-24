d05650c2213e5ad73f1f5b7721f900fd86419360
Travis Miller <travis@travismiller.com>
2019-03-24 01:42:19 -0500

    don't purge tailwind ':' classes

--- zola/purgecss.config.js
+++ zola/purgecss.config.js
@@ -1,11 +1,29 @@
+/*
+Custom extractor for purgeCSS to avoid
+stripping classes with `:` prefixes
+@see https://egghead.io/lessons/tailwind-removing-unused-css-in-tailwind-with-purgecss
+*/
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   content: [
     './public/**/*.html',
+    './public/**/*.js',
   ],
   css: [
     './public/css/**/*.css',
   ],
+  extractors: [
+    {
+      extractor: TailwindExtractor,
+      extensions: ['html', 'js']
+    }
+  ],
   whitelistPatterns: [
-    /^js-/,
+    // /^js-/,
   ],
 }
